0c5fdbcb7c0d68b7650d5d3092448874195b228f
Félix MARQUET <fpmovie4@gmail.com>
2025-01-27 20:00:26 +0100

    first commit

--- src/routes/github/github.ts
+++ src/routes/github/github.ts
@@ -5,10 +5,37 @@ const router = express.Router();
 
 dotenv.config();
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
+        const { title, body, labels, assignees } = req.body;
         const githubIssueToken = process.env.GITHUB_ISSUE_TOKEN;
-        res.status(200).json({ token: githubIssueToken });
+        // Vérifier si le token est défini
+        if (!githubIssueToken) {
+            res.status(500).json({ message: 'Internal server error: Github token is not defined' });
+            return;
+        }
+        const response = await fetch("https://api.github.com/repos/appen-isen/isen-orbit/issues", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+                "Authorization": `Bearer ${githubIssueToken}`,
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                labels,
+                assignees
+            }),
+        });
+        console.log(response);
+        if (response.status !== 201) {
+            res.status(500).json({ message: 'Internal server error: Unable to create issue' });
+            return;
+        } else {
+            res.status(200).json({ message: 'Issue created successfully' });
+        }
     } catch (error) {
         res.status(500).json({ message: 'Internal server error ' + error });
     }
