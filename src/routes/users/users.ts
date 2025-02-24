5304e7ff204aed4d2f80660d443f245b0c0927d8
Félix MARQUET <fpmovie4@gmail.com>
2025-02-24 10:47:19 +0100

    feat(users): ajouter la fonctionnalité de récupération des utilisateurs par email et création de la table des utilisateurs

--- src/routes/users/users.ts
+++ src/routes/users/users.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
-import {getAllUsers} from "./getUsers";
+import { 
+    getAllUsers, getUserByEmail } from "./getUsers";
 import {postUsers} from "./postUsers";
 import {putUsers} from "./putUsers";
 import {deleteUsers} from "./deleteUsers";
@@ -12,16 +13,30 @@ router.get("/", async (req, res) => {
         const users = await getAllUsers();
         res.status(200).json({ message: users });
     } catch (error) {
+        console.error(error)
         res.status(500).json({ message: 'Internal server error ' + error });
     }
 });
 
-router.post("/", async (req, res) => {
+router.get("/:email", async (req, res) => {
     try {
-        const user = req.body;
+        const email = req.params.email;
+        const user = await getUserByEmail(email);
+        res.status(200).json({ message: user });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal server error ' + error });
+    }
+});
+
+router.post("/:email", async (req, res) => {
+    console.log(req.params.email);
+    try {
+        const user = req.params.email;
         const newUser = await postUsers(user);
         res.status(200).json({ message: newUser });
     } catch (error) {
+        console.error(error)
         res.status(500).json({ message: 'Internal server error ' + error });
     }
 });
@@ -33,6 +48,7 @@ router.put("/:id", async (req, res) => {
         const updatedUser = await putUsers(id, user);
         res.status(200).json({ message: updatedUser });
     } catch (error) {
+        console.error(error)
         res.status(500).json({ message: 'Internal server error ' + error });
     }
 });
@@ -43,6 +59,7 @@ router.delete("/:id", async (req, res) => {
         const deletedUser = await deleteUsers(id);
         res.status(200).json({ message: deletedUser });
     } catch (error) {
+        console.error(error)
         res.status(500).json({ message: 'Internal server error ' + error });
     }
 });
